b6fbbcec35b70ba126f9ed336f4286c6660cc47d
王宏伟 <wanghongwei@hualala.com>
2024-03-27 16:42:30 +0800

    test：eslint4

--- src/router/set.js
+++ src/router/set.js
@@ -13,7 +13,7 @@ import Page403 from '../view/common/403.js'
 import { axiosGetUserInfo, axiosGetUserRight } from '../service/apis/common'
 
 let userRightList = [] // 用户权限列表
-let platRoutesf = [] // 扁平化的路由，方便title设置
+let platRoutesfd = [] // 扁平化的路由，方便title设置
 let A = []
 
 function SetRouter () {
