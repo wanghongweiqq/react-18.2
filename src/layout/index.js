b9149c2c0add9555fc7902aa4314e52d1196d1fd
王宏伟 <wanghongwei@hualala.com>
2024-04-02 09:59:58 +0800

    test: lint-staged

--- src/layout/index.js
+++ src/layout/index.js
@@ -2,7 +2,7 @@
  * @Author: 王宏伟
  * @Email：wanghongwei@hualala.com
  * @Date: 2024-03-15 10:42:38
- * @Description: 页面/组件/功能的描述
+ * @Description: 页面公共部分
  * @FilePath: /react-18.2/src/layout/index.js
  */
 import React from 'react'
